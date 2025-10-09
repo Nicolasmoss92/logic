fb96648bc90e7aca8375b31138c0d0f0080019d2
00043963 <00043963@randon.com.br>
2025-10-09 06:54:52 -0300

    soma de todos os numeros de um array

--- if/else/easy/for/while/number1.js
+++ if/else/easy/for/while/number1.js
@@ -1,8 +1,6 @@
 //Contar Pares e Ímpares: Crie uma função que recebe um array de números e retorna um objeto com a contagem de números pares e ímpares.
 //FUCK OFF COPILOT
 
-
-
 function someParesImpares(numbers) {
     let saveNumbersPar = [];
     let saveNumbersImpar = [];
@@ -10,16 +8,16 @@ function someParesImpares(numbers) {
     for (var i = 0; i < numbers.length; i++) {
         if (numbers[i] % 2 === 0) {
             saveNumbersPar.push(numbers[i]);
-        } if (numbers[i] % 2 === 1) {
-            saveNumbersImpar.push(numbers[i])
+        } else {
+            saveNumbersImpar.push(numbers[i]);
         }
-    }
-    const numbersObjects = {
+    };
+
+    return numbersObjects = {
         "pares": saveNumbersPar.length,
         "impares": saveNumbersImpar.length
-    }
-    return numbersObjects;
-}
+    };
+};
 
 
-console.log(someParesImpares([1, 2, 4]));
+console.log(someParesImpares([1, 2, 4, 6, 7, 8]));
